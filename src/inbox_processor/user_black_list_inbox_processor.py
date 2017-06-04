3ec1f43a3faa365ea492e33f4c6bbb46ec672444
Shade <shadetheartist@gmail.com>
2017-06-04 00:44:26 -0700

    improving the blacklist processor to handle more scenarios

--- src/inbox_processor/user_black_list_inbox_processor.py
+++ src/inbox_processor/user_black_list_inbox_processor.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from src.inbox_processor.base_inbox_processor import BaseInboxProcessor
 from src.poster.base_poster import BasePoster
 from src.storage.base_storage import BaseStorage
+from pprint import pprint
+
 
 class UserBlacklistInboxProcessor(BaseInboxProcessor):
 
@@ -13,13 +15,49 @@ class UserBlacklistInboxProcessor(BaseInboxProcessor):
         print('Processing inbox for users to blacklist')
         for comment in self.reddit.inbox.unread():
             if 'blacklist me' in comment.body.lower():
-                self.reply_to_blacklist(comment)
+                self.process_user_self_blacklist_request(comment)
+            elif 'blacklist me' in comment.subject.lower():
+                self.process_user_self_blacklist_request(comment)
+            elif 'fucking blacklist' in comment.subject.lower():
+                self.process_mod_bulk_blacklist_request(comment)
+            elif 'fucking blacklist' in comment.subject.lower():
+                self.process_mod_blacklist_request(comment)
 
-    def reply_to_blacklist(self, comment):
-
+    def process_user_self_blacklist_request(self, comment):
         user = comment.author.name
         self.storage.blacklist_user(user)
         comment.reply('It is done my lord. You have been blacklisted from /r/both_sides_bot username mentions.')
-        comment_list = list()
-        comment_list.append(comment)
-        self.reddit.inbox.mark_read(comment_list)
+        self.reddit.inbox.mark_read([comment])
+
+    def validate_mod_blacklist(self, comment):
+        if comment.author.name == 'both_sides_blacklist':
+            return True
+
+        if comment.author.name == 'both_sides_bot':
+            return True
+
+    def process_mod_blacklist_request(self, comment):
+        self.validate_mod_blacklist(comment)
+
+        blacklist_list_str = comment.body.lower()
+        blacklist_list_str_words = blacklist_list_str.split(' ')
+        
+        # fucking blacklist [2](username)
+        user_to_blacklist = blacklist_list_str[2]
+
+        #4 space prefix makes reddit format in monospace (like the robot i am >:I)
+        comment.reply('    Motherfucker just GOT blacklisted my FRIEND. WOO.')
+
+        self.reddit.inbox.mark_read([comment])
+        
+    def process_mod_bulk_blacklist_request(self, comment):
+        self.validate_mod_blacklist(comment)
+
+        blacklist_list_str = comment.body.lower()
+        blacklist_list = blacklist_list_str.split(', ')
+
+        for username in blacklist_list:
+            self.storage.blacklist_user(username)
+            comment.reply('Dem motherfuckers just got BLACK LISTED my DUDE.')
+            self.reddit.inbox.mark_read([comment])
+        
